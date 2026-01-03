f1d24308c82d93618ac229670f48445b5f2790f6
bsangram17-design <bsangram17@gmail.com>
2026-01-03 15:50:38 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = var.region
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.27"
+
+  vpc_id     = "vpc-xxxxxxxx"
+  subnet_ids = ["subnet-xxxx", "subnet-yyyy"]
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+      instance_types = ["t3.medium"]
+    }
+  }
+}
