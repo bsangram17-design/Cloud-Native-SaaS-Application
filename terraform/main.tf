829d18dabb201a8045eda5664ee3f657231f3f45
bsangram17-design <bsangram17@gmail.com>
2026-01-05 16:10:10 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "saas-cluster"
+  name    = "saas-cluster"
   
   vpc_id     = "vpc-05a566183852fd498"
   subnet_ids = ["subnet-0b5d9483c9f20dac4", "subnet-0f939c3bd6278333f"]
